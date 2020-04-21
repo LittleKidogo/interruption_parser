bce5c561baf2044205baa5546f6529d209a07bf7
Pancakem <pancakesdeath@protonmail.com>
2020-04-21 18:16:00 +0300

    Validate api input

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,10 @@
 import os
 from flask import Flask, request, jsonify
-from api.config import app_config
-from parser.parser import parse
 import requests
 from multiprocessing import Process
+from api.config import app_config
+from parser.parser import parse
+from api.util import validate_url
 
 document_url = None
 callback_url = None
@@ -22,13 +23,16 @@ class ParseProcess(Process):
 def run_parse():
     global document_url, callback_url
     data = parse(document_url)
+    headers = {'Content-Type': 'application/json'}
+    if data is None:
+        requests.post(callback_url, headers=headers, data={'error' : 'couldn\'t parse the requested document', data: None})
+        return
     response = {
             'error': 'None',
             'data': data
         }
-    header = {'Content-Type': 'application/json'}
-    r = requests.post(callback_url, headers=header, data=response)
-    print(r.status)
+    requests.post(callback_url, headers=headers, data=response)
+
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -38,10 +42,18 @@ def create_app(config_name):
 
     @app.route('/', methods=['POST'])
     def get_document_link():
-        p = ParseProcess(run_parse)
         global document_url, callback_url
+        p = ParseProcess(run_parse)
+
         document_url = request.json['document_url']
         callback_url = request.json['callback_url']
+
+        if type(document_url) is not str or type(callback_url) is not str:
+            return jsonify({'error': 'invalid types. input must be string', 'status': 'fail'}), 409
+
+        elif not validate_url(document_url) or not validate_url(callback_url):
+            return jsonify({'error': 'invalid urls', 'status': 'fail'}), 409
+
         p.start()
         return jsonify({'error': None, 'status': 'Ok'}), 200
 

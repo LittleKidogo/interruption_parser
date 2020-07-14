e2728cd657cec6ccb343361ca161b1724416b828
Pancakem <pancakesdeath@protonmail.com>
2020-07-14 15:38:14 +0300

    Remove leading and trailing dots

--- parser/parser.py
+++ parser/parser.py
@@ -2,10 +2,8 @@ import requests
 from pdfminer.high_level import extract_text
 from shutil import copyfileobj
 import tempfile
-import re
-
 from re import search, sub, IGNORECASE
-
+from .util import rlstrip_dot, composite_function
 
 # {
 # 	"region": {
@@ -37,6 +35,7 @@ def get_text(url):
     text = sub(r"[\s]{2,}", ' ', text)
     return text
 
+
 def get_regions(text):
     regions = dict()
     regex = r"[.]([a-zA-Z\s]+?REGION)(.+?)[.](?:[a-zA-Z\s]+?REGION)"
@@ -45,7 +44,6 @@ def get_regions(text):
         # Get the top regio
         region = dict()
         region["name"] = region_search.group(1).strip()
-        print("Region: " + region["name"])
         region_key = '_'.join(region["name"].lower().split(' '))
         region["counties"] = get_counties(region_search.group(2), regions, region_key)
         regions[region_key] = region
@@ -61,7 +59,6 @@ def get_regions(text):
         # Get the last region
         region = dict()
         region["name"] = last_region_check.group(1).strip()
-        print("Region: " + region["name"])
         region_key = '_'.join(region["name"].lower().split(' '))
         region["counties"] = get_counties(last_region_check.group(2), regions, region_key)
         regions[region_key] = region
@@ -75,7 +72,6 @@ def get_counties(text, regions, region_key):
         # Get the top county
         county = dict()
         county["name"] = county_search.group(1).strip()
-        print("County: " + county["name"])
         county["areas"] = get_areas(county_search.group(2))
 
         # Check if the region already exists
@@ -96,7 +92,6 @@ def get_counties(text, regions, region_key):
         # Get the last county
         county = dict()
         county["name"] = last_county_check.group(1).strip()
-        print("County: " + county["name"])
         county["areas"] = get_areas(last_county_check.group(2))
 
         # Check if the region already exists
@@ -115,7 +110,6 @@ def get_areas(text):
 		# Get the top area
 		area = dict()
 		area["name"] = area_search.group(1)
-		print(area["name"])
 		area["details"] = get_details(area_search.group(2))
 		areas.append(area)
 
@@ -131,38 +125,32 @@ def get_areas(text):
 		# Get the last area
 		area = dict()
 		area["name"] = last_area_check.group(1)
-		print(area["name"])
 		area["details"] = get_details(last_area_check.group(2))
 		areas.append(area)
 
-	print("\n")
 	return areas
 
 def get_details(text):
 	details = dict()
 	date_search = search(r"(DATE:)(.+?)TIME", text, IGNORECASE)
 	if date_search:
-		print(date_search.group(2).strip())
 		details["date"] = date_search.group(2).strip()
 		text = text.replace(date_search.group(1), '')
 		text = text.replace(date_search.group(2), '')
 
 	time_search = search(r"(TIME:)(.+?P[.]M[.])", text, IGNORECASE)
 	if time_search:
-		print(time_search.group(2).strip())
 		details["time"] = time_search.group(2).strip()
 		text = text.replace(time_search.group(1), '')
 		text = text.replace(time_search.group(2), '')
 
 	details["locations"] = get_locations(text)
-	print(details["locations"])
 
-	print("\n")
 	return details
 
 def get_locations(text):
 	stripSpaces = lambda location : location.strip()
-	return list(map(stripSpaces, text.split(',')))
+	return list(map(composite_function(stripSpaces, rlstrip_dot), text.split(',')))
 
 def parse(url):
     return get_regions(get_text(url))
